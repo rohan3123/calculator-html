5c5d3eb4181a21fffbb269a46e4f35aadb4109e0
Rohan <rohangazi30@gmail.com>
2023-08-27 21:57:58 +0100

    adding symbol functionality

--- script.js
+++ script.js
@@ -28,6 +28,62 @@ function handleSymbol(symbol){
             buffer=runningTotal;
             runningTotal=0;
             break;
-        case ''
+        case '←':
+            if(buffer.length === 1){
+                buffer = '0';
+            }else{
+                buffer = buffer.toString(0, buffer.length - 1);
+            }
+            break;
+        case '+':
+        case '-':
+        case 'x':
+        case '÷':
+            handleMath(symbol);
+            break;
     }
+}
+
+function handleMath(symbol){
+    if(buffer === '0'){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    perviousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer){
+    if(perviousOperator === '+'){
+        runningTotal += intBuffer;
+
+    }else if(perviousOperator === '-'){
+        runningTotal -= intBuffer;
+    }else if(perviousOperator === 'x'){
+        runningTotal *= intBuffer;
+    }else if(perviousOperator === '÷'){
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleNumber(numberString){
+    if(buffer === "0") {
+        buffer = numberString;
+    }else{
+        buffer +=numberString;
+    }
+}
+
+function init(){
+    document.querySelector('.calc-buttons').addEventListener('click', function(event{
+
+        
+    }))
 }
